5019a6364c1d96ca5c89d7d6857b79022ced76fa
trissentety <82165867+trissentety@users.noreply.github.com>
2023-08-23 03:47:42 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+array1 = [1,2,3,4,5]
+array3 = []
+array3.push([array1.length -1])
+console.log(array3)
+console.log(array3.toString())
+array3.push([array1.length - 1])
+console.log(array3)
+// array1 = [1,2,3,4];
+// array2 = [1,2,3,4];
+// if (array1.length === array2.length) {
+// console.log("yes")
+// }
